4917887db4f05a3ca576d238cbe803f2fb26d426
Enoch Okine Mensah <okaycode100@gmail.com>
2025-09-16 20:13:11 +0000

    Using components, typing out some funtional components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Landing: React.FC = () => {
+import Card from "@/components/Card"
+import Button from "@/components/Button"
+const Landing: React.FC =  () => {
   return (
     <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Button title="Small Button" styles="rounded-sm" />
+      <Button title="Medium Button" styles="rounded-md" />
+      <Button title="Large Button" styles="rounded-full lg:rounded-lg" />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
