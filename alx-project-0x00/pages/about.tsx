a7489666c00784d0d0903df4661ac6b78b14416a
Enoch Okine Mensah <okaycode100@gmail.com>
2025-09-16 19:11:29 +0000

    amended landing and about page files

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">About</h1>
+    </div>
+  )
+}
+export default About
